377666797930e1a19b11cb3725b60bd1cbca0a5b
“Rebecca <rebeccaschiffman@gmail.com>
2022-02-23 20:14:21 -0800

    Change book card creation to a function with for loop to display everythin in the array

--- script.js
+++ script.js
@@ -2,8 +2,18 @@
 
 // VARIABLES
 
-//The array where book objects are stored
+//The array where book objects are stored - empty version
 let myLibrary = [];
+
+// The array where books objects are stored - starting with one default book
+// let myLibrary = [ bookPrototype = {
+//     title = 'The Hobit',
+//     author = 'J.R. Tolkien',
+//     pageCount = 200,
+//     readStatus = 'read',
+// }]
+
+
 
 //Book card divs are placed inside this container
 const cardcontainer = document.querySelector('#cardcontainer');
@@ -42,23 +52,58 @@ function hideElement(ID){
     document.getElementById(ID).style.display ='none';
 }
 
+// Function to remove all children elements
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+
+//This version works by being called inside saveNewBook 
+//but instead I need a way to loop over the myLibrary array and 
+//make cards from each item in array rather than the newly created object.
 // Function to create a book card with details from book object
 // and put the card in the card container.
 // https://www.w3schools.com/jsref/met_node_appendchild.asp
-const newBookCard = function(book){
+
+// const newBookCard = function(book){
+//     const bookCard = document.createElement("div");
+//     bookCard.classList.add( "bookCard" );
+//     bookCard.textContent=
+//     `Title: "${book.title}"\r\n
+//     Author: ${book.author}\r\n
+//     Pages: ${book.pageCount}\r\n 
+//     Status: ${book.readStatus}   
+//     `;
+//     document.getElementById("cardcontainer").appendChild(bookCard);
+//     }
+
+//end old version of newBookCard
+
+//  for each start array.forEach((item) => {
+// for version(let i = 0, i = array.length-1, i++)
+// new version is now newCards since it adds all of them
+const newCards = function(array){
+    console.log('newCards function is running')
+    console.log([array]);
+   
+    
+    removeAllChildNodes(cardcontainer);
+
+   for (let i = 0; i < array.length; i++) {
+       console.log(array[i].author);
+    
     const bookCard = document.createElement("div");
-    bookCard.classList.add( "bookCard" );
-    bookCard.textContent=
-    `Title: "${book.title}"\r\n
-    Author: ${book.author}\r\n
-    Pages: ${book.pageCount}\r\n 
-    Status: ${book.readStatus}   
-    `;
-    // const textnode = document.createTextNode("Water");
-    // console.log(textnode);
-    // bookCard.appendChild(textnode);
-    document.getElementById("cardcontainer").appendChild(bookCard);
-    }
+        bookCard.classList.add( "bookCard" );
+        bookCard.textContent=
+        `Title: ${array[i].title}\r\n
+        Author: ${array[i].author}\r\n
+        Pages: ${array[i].pageCount}\r\n 
+        Status: ${array[i].readStatus}   
+        `;
+        document.getElementById("cardcontainer").appendChild(bookCard);
+    };}
     
 
 function saveNewBook(){
@@ -71,13 +116,13 @@ function saveNewBook(){
     myLibrary.push(newBook);
     console.log(newBook);
     console.log([myLibrary]);
-    newBookCard(newBook);
+    // newBookCard(newBook);
+    newCards(myLibrary);
     console.log(readStatus.value);
     document.getElementById("newBookForm").reset();
     hideElement("newBookFormContainer");
 }
 
-//This does not work
 // function to set a book's read value based on checkbox input
 const setReadStatus = function(){
     if (readValue.checked){
